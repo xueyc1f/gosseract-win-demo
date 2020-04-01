07765534a16ad1ca1506101c3fde03127f8750b3
xuexiantao <xxt@nosugartech.com>
2020-04-01 17:03:32 +0800

    update...

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"git.nosugar.io/gosseract"
-	"os"
+	"log"
 )
 
 func main() {
-	os.Setenv("TESSDATA_PREFIX", `./gosseract`)
+	log.Println(gosseract.Version())
 	client := gosseract.NewClient()
 	defer client.Close()
 	client.SetImage("./test.png")
 	client.SetLanguage("eng")
 	client.SetPageSegMode(gosseract.PSM_SINGLE_BLOCK)
 	text, err := client.Text()
-	fmt.Println(text, err)
-	fmt.Println(gosseract.Version())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(text)
 }
